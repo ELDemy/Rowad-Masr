ed675b5048219457608f01f349db22a9ca310293
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-08-22 16:03:46 +0300

    initiate main

--- flutter/dart/bin/dart_tasks.dart
+++ flutter/dart/bin/dart_tasks.dart
@@ -1,3 +1,11 @@
+import '../session_1/1.dart';
+import '../session_1/2.dart';
+
 void main() {
-  print("Hello World");
+  // 1
+  ListOfNumbers list = new ListOfNumbers(numbersList: [12, 15, 13, 3]);
+  // print(list.product());
+  // 2
+  Rectangle rectangle = new Rectangle(height: 6, width: 4);
+  print(rectangle.perimeter());
 }
---
+++ flutter/dart/session_1/4.dart
@@ -0,0 +1,9 @@
+/*
+4.
+Write a program that calculates and prints both the arithmetic and the harmonic
+mean, Of the two integers x and y, where x=12 and y—13.
+2
+Harmonic mean =
+Arithmetic mean =
+2
+ */
