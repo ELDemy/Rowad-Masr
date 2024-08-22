9c45fb047926dae7274730cf3291c3652a2d2bc1
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-08-22 16:23:25 +0300

    Q5

--- flutter/dart/bin/dart_tasks.dart
+++ flutter/dart/bin/dart_tasks.dart
@@ -1,11 +1,12 @@
-import '../session_1/1.dart';
-import '../session_1/2.dart';
+import '../session_1/5.dart';
 
 void main() {
-  // 1
-  ListOfNumbers list = new ListOfNumbers(numbersList: [12, 15, 13, 3]);
-  // print(list.product());
-  // 2
-  Rectangle rectangle = new Rectangle(height: 6, width: 4);
-  print(rectangle.perimeter());
+  // // 1
+  // ListOfNumbers list = new ListOfNumbers(numbersList: [12, 15, 13, 3]);
+  // print(list.harmonicMean(number1Index: 0, number2Index: 2));
+  // // 2
+  // Rectangle rectangle = new Rectangle(height: 6, width: 4);
+  // print(rectangle.perimeter());
+// 5
+  applesQ();
 }
---
+++ flutter/dart/session_1/5.dart
@@ -0,0 +1,17 @@
+/*
+.5
+Write a program that asks the user to input a number of apples, then tells him
+how many dozens of apples he has and how many extra apples are left over.
+(Test your program using different values such as 21, 36 and 8 apples). Hint:
+Use the division and modulus operators taking advantage of the truncation when integer
+division is performed. (This hint also goes to questions 7, 8, 9 and IO).
+ */
+import 'dart:io';
+
+void applesQ() {
+  int applesCount = int.parse(stdin.readLineSync()!);
+  int dozens = (applesCount / 12).toInt();
+  int leftOver = applesCount % 12;
+
+  print("You have ${dozens} dozens of apple \nAnd ${leftOver} left over");
+}
