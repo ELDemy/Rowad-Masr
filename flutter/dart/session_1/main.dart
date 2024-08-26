4c168d76ae48f648e91417bf7dc7c1015bc46b69
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-08-26 13:29:33 +0300

    initiate Task 2

--- flutter/dart/session_1/main.dart
+++ flutter/dart/session_1/main.dart
@@ -1,12 +1,12 @@
-import '../session_1/7.dart';
+import '7.dart';
 
 void main() {
   // // 1
   // ListOfNumbers list = new ListOfNumbers(numbersList: [12, 15, 13, 3]);
   // print(list.harmonicMean(number1Index: 0, number2Index: 2));
   // // 2
-  // Rectangle rectangle = new Rectangle(height: 6, width: 4);
-  // print(rectangle.perimeter());
+  // double rectanglePerimeter = new Rectangle(height: 6, width: 4).perimeter();
+  // print(rectanglePerimeter);
 // 5
 //   applesQ();
 
