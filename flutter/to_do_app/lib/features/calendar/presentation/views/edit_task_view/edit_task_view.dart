55ec36d3c219458b17cfb62f7b31f6b0cf932f66
MELDemy <mahmoudeldemerdash5@gmail.com>
2024-09-25 20:56:31 +0300

    add edit task View

---
+++ flutter/to_do_app/lib/features/calendar/presentation/views/edit_task_view/edit_task_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EditTaskView extends StatelessWidget {
+  const EditTaskView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SizedBox(),
+    );
+  }
+}
--- flutter/to_do_app/lib/features/calendar/presentation/views/widgets/tasks_list/task_card/task_card.dart
+++ flutter/to_do_app/lib/features/calendar/presentation/views/widgets/tasks_list/task_card/task_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 import 'package:to_do_app/core/models/task_model.dart';
 import 'package:to_do_app/core/state_management/tasks_cubit/tasks_cubit.dart';
 import 'package:to_do_app/core/utiles/app_colors.dart';
+import 'package:to_do_app/features/calendar/presentation/views/edit_task_view/edit_task_view.dart';
 import 'package:to_do_app/features/calendar/presentation/views/widgets/tasks_list/task_card/task_props.dart';
 import 'package:to_do_app/helper/datetime_extension.dart';
 
@@ -20,51 +22,66 @@ class _TaskCardState extends State<TaskCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 16),
-      padding: const EdgeInsets.only(top: 12, right: 10, left: 10, bottom: 4),
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(4)),
-        color: AppColors.greyBackgroundColor,
-      ),
-      child: Row(
-        children: [
-          IconButton(
-            isSelected: taskModel.isCompleted,
-            padding: EdgeInsets.zero,
-            constraints: const BoxConstraints(),
-            style: IconButton.styleFrom(
-              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-            ),
-            onPressed: () {
-              setState(() {
-                BlocProvider.of<TasksCubit>(context).completeTask(taskModel);
-              });
-            },
-            icon: const Icon(Icons.circle_outlined),
-            selectedIcon:
-                const Icon(Icons.circle, color: AppColors.purplePrimaryColor),
-            color: Colors.white.withOpacity(.87),
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: InkWell(
+        onTap: () => PersistentNavBarNavigator.pushNewScreen(
+          context,
+          screen: const EditTaskView(),
+          withNavBar: false,
+          pageTransitionAnimation: PageTransitionAnimation.fade,
+        ),
+        child: Container(
+          padding:
+              const EdgeInsets.only(top: 12, right: 10, left: 10, bottom: 4),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            color: AppColors.greyBackgroundColor,
           ),
-          const SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _taskTitle(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Row(
+            children: [
+              _taskCircleIcon(context),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    _taskTime(),
-                    TaskProps(taskModel: taskModel),
+                    _taskTitle(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _taskTime(),
+                        TaskProps(taskModel: taskModel),
+                      ],
+                    )
                   ],
-                )
-              ],
-            ),
-          )
-        ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  IconButton _taskCircleIcon(BuildContext context) {
+    return IconButton(
+      isSelected: taskModel.isCompleted,
+      padding: EdgeInsets.zero,
+      constraints: const BoxConstraints(),
+      style: IconButton.styleFrom(
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      ),
+      onPressed: () {
+        setState(() {
+          BlocProvider.of<TasksCubit>(context).completeTask(taskModel);
+        });
+      },
+      icon: const Icon(Icons.circle_outlined),
+      selectedIcon:
+          const Icon(Icons.circle, color: AppColors.purplePrimaryColor),
+      color: Colors.white.withOpacity(.87),
     );
   }
 
