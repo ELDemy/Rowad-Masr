2eed535ced796124e8226e4d9794ec6383d49d98
ELDemy <mahmoudeldemerdash5@gmail.com>
2025-07-11 21:04:27 +0300

    APP USIN MVVM only

--- flutter/news_app/lib/main.dart
+++ flutter/news_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/views/home_view/home_view.dart';
 
 void main() {
   runApp(const NewsApp());
@@ -12,25 +13,6 @@ class NewsApp extends StatelessWidget {
     return const MaterialApp(
       title: "News App",
       home: HomeView(),
-    );
-  }
-}
-
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: const Text("Title"),
-            trailing: Transform.flip(
-                flipX: true, child: const Icon(Icons.arrow_back_ios)),
-          );
-        },
-      ),
     );
   }
 }
